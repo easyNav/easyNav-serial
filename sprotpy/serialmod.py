acfd1d6270d9f03ddad9a1a8ea7ad26d149d5235
adnan77 <v2102ap@gmail.com>
2014-10-24 10:08:22 +0800

    update serialmod

--- sprotpy/serialmod.py
+++ sprotpy/serialmod.py
@@ -2,7 +2,7 @@
 import sprotapi as sprotapi
 from easyNav_pi_dispatcher import DispatcherClient
 
-DATA_SIZE = 5
+DATA_SIZE = 16
 DEST_PORT_CRUNCHER = 9003
 DEST_PORT_ALERT = 9004
 
@@ -10,8 +10,9 @@ def convertPacketToSonarData(strpkt):
     sonarData = { strpkt[0] : strpkt[2:5] }
     return sonarData
 
-sprotapi.SPROTInit("/dev/ttyAMA0", baudrate=57600)
+sprotapi.SPROTInit("/dev/ttyAMA0", baudrate=115200)
 dc = DispatcherClient(port=9005)
+dc.start()
 
 sonar1Data = 0
 sonar2Data = 0
@@ -30,17 +31,21 @@ while True :
 			sonar1Data = convertPacketToSonarData(strpkt)
 		elif (strpkt[0] == b'2') :
 			sonar2Data = convertPacketToSonarData(strpkt)
-        	elif (strpkt[0] == b'C') :
+        elif (strpkt[0] == b'C') :
 			compassData = { "angle" : strpkt[2:5] }
 			dc.send(DEST_PORT_CRUNCHER, "angle", compassData)
-			print "Compass data to cruncher ", compassData
-			
+			print "Compass ==> cruncher ", compassData
+		elif (strpkt[0] == b'F') :
+			footsensData = { "footsens" : strpkt[2:15] }
+			#dc.send(DEST_PORT_CRUNCHER, "footsens", footsensData)
+			print "Foot sensor ==> ? ", footsensData
+		
 		# If both sonar data are available, combine them and send to alert
 		if ((sonar1Data != 0) and (sonar2Data != 0)) :
 			sonar1Data.update(sonar2Data)
 			combinedSonarData = sonar1Data
 			dc.send(DEST_PORT_ALERT, 'sonarData', combinedSonarData)
-			print "Sonar data to alert ", combinedSonarData
+			print "Sonar ==> alert ", combinedSonarData
 			sonar1Data = 0
 			sonar2Data = 0
 
