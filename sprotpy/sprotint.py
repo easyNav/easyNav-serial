d03f3a68c461c16413489b16d654333785971ef3
adnan77 <v2102ap@gmail.com>
2014-10-30 08:59:19 +0800

    updated serial

--- sprotpy/sprotint.py
+++ sprotpy/sprotint.py
@@ -5,17 +5,14 @@ import sprotapi as sprotapi
 import sprotpkt as sprotpkt
 
 
-def createPacket(dtBit, packetType, data):
+def createPacket(data):
     
     rawPacket = bytearray(sprotcfg.PACKET_SIZE)
-    rawPacket[sprotcfg.FIELD_TYPE_OFFSET] = packetType
-    rawPacket[sprotcfg.FIELD_DT_OFFSET] = dtBit
+    rawPacket[sprotcfg.FIELD_PM_OFFSET] = sprotcfg.SPROT_PROTOCOL_MARKER
     rawPacket[sprotcfg.FIELD_DLEN_OFFSET] = len(data);
     rawPacket[sprotcfg.FIELD_DATA_OFFSET:sprotcfg.FIELD_DATA_OFFSET+len(data)] = data
+	rawPacket[sprotcfg.FIELD_CHECKSUM_OFFSET] = generateChecksum(packet)
     packet = sprotpkt.SPROTPacket(rawPacket)
-
-    # Generate checksum for this packet
-    packet.checksum = generateChecksum(packet)
 
     return packet
 
@@ -37,7 +34,7 @@ def receivePacket(timeout=sprotcfg.SPROT_RECV_TIMEOUT):
     recvBytes = bytearray(sprotapi.serialPort.read(sprotcfg.PACKET_SIZE))
         
     if (len(recvBytes) < sprotcfg.PACKET_SIZE) :
-        return sprotcfg.SPROT_ERROR_TIMEOUT
+        return sprotcfg.SPROT_ERROR
     
     packet = sprotpkt.SPROTPacket(recvBytes)
             
@@ -45,57 +42,10 @@ def receivePacket(timeout=sprotcfg.SPROT_RECV_TIMEOUT):
     checksum = generateChecksum(packet)
 
     if (checksum != packet.checksum):    
-        return sprotcfg.SPROT_ERROR_CHECKSUM
+        return sprotcfg.SPROT_ERROR
     else :
         return packet
     
 
 def sendPacket(packet):
     sprotapi.serialPort.write(packet.toByteArray())
-
-
-def sendControlPacket(packetType):
-    controlPacket = bytearray(sprotcfg.PACKET_SIZE)
-    controlPacket[sprotcfg.FIELD_TYPE_OFFSET] = packetType
-    controlPacket[sprotcfg.FIELD_DLEN_OFFSET] = 0
-    controlPacket[sprotcfg.FIELD_CHECKSUM_OFFSET] = packetType
-    sprotapi.serialPort.write(controlPacket)
-
-
-def startSession(timeout):
-    doRetry = True
-    retries = 0
-
-    while(doRetry):
-        sendControlPacket(sprotcfg.PACKET_TYPE_HELLO)
-        recv = receivePacket(timeout)
-
-        if (isinstance(recv, sprotpkt.SPROTPacket) and (recv.type == sprotcfg.PACKET_TYPE_ACK)):
-            return sprotcfg.SPROT_ERROR_NONE
-        elif (recv == -1):
-            return -1
-        else :
-            retries += 1
-            doRetry = False if (retries > sprotcfg.MAX_RETRIES_SESSION_INIT) else 1
- 
-    return -1
-
-
-def waitForSessionStart(timeout):
-    doRetry = True
-    retries = 0
-
-    while (doRetry):  
-        recv = receivePacket(timeout)
-
-        if (isinstance(recv, sprotpkt.SPROTPacket) and (recv.type == sprotcfg.PACKET_TYPE_HELLO)):     
-            sendControlPacket(sprotcfg.PACKET_TYPE_ACK)
-            return sprotcfg.SPROT_ERROR_NONE        
-        elif (recv == -1):       
-            return -1       
-        else :      
-            retries += 1
-            doRetry = False if (retries > sprotcfg.MAX_RETRIES_SESSION_INIT) else 1
-   
-    return -1
-
--- sprotpy/sprotpkt.py
+++ sprotpy/sprotpkt.py
@@ -9,35 +9,29 @@ import sprotcfg as sprotcfg
 
 class SPROTPacket:
     def __init__(self, rawPacket=None):
-        self.dtBit = 0;
-        self.type = 0
-        self.dataLength = 0;
-        self.data = None
-        self.checksum = 0
-        
         if(rawPacket == None):
-            return
-        
-        # Construct packet from given array of bytes
-        self.type = rawPacket[sprotcfg.FIELD_TYPE_OFFSET]
-        self.dtBit = rawPacket[sprotcfg.FIELD_DT_OFFSET]
-        self.dataLength = rawPacket[sprotcfg.FIELD_DLEN_OFFSET]
-        self.data = bytearray(rawPacket[sprotcfg.FIELD_DATA_OFFSET:sprotcfg.FIELD_DATA_OFFSET + self.dataLength])
-        self.checksum = rawPacket[sprotcfg.FIELD_CHECKSUM_OFFSET]
-        self.raw = rawPacket
-        
-    
+			self.protocolMarker = sprotcfg.SPROT_PROTOCOL_MARKER
+			self.dataLength = 0;
+			self.data = None
+			self.checksum = 0
+			self.raw = None
+        else :
+			# Construct packet from given array of bytes
+			self.protocolMarker = [sprotcfg.FIELD_PM_OFFSET]
+			self.dataLength = rawPacket[sprotcfg.FIELD_DLEN_OFFSET]
+			self.data = bytearray(rawPacket[sprotcfg.FIELD_DATA_OFFSET:sprotcfg.FIELD_DATA_OFFSET + self.dataLength])
+			self.checksum = rawPacket[sprotcfg.FIELD_CHECKSUM_OFFSET]
+			self.raw = rawPacket
+     
     def toByteArray(self):
         self.raw = bytearray(sprotcfg.PACKET_SIZE)
-        self.raw[sprotcfg.FIELD_TYPE_OFFSET] = self.type
-        self.raw[sprotcfg.FIELD_DT_OFFSET] = self.dtBit       
+        self.raw[sprotcfg.FIELD_PM_OFFSET] = sprotcfg.SPROT_PROTOCOL_MARKER
         self.raw[sprotcfg.FIELD_DLEN_OFFSET] = len(self.data);
         self.raw[sprotcfg.FIELD_DATA_OFFSET:sprotcfg.FIELD_DATA_OFFSET+len(self.data)] = self.data
         self.raw[sprotcfg.FIELD_CHECKSUM_OFFSET] = self.checksum
         return self.raw
 
-
     def printPacket(self):
-        print "pktinfo :  toggle=", self.dtBit, "  dlen=", self.dataLength, "  data=", self.data, "  checksum=", format(self.checksum, "02x")
+        print "pktinfo : DLEN=", self.dataLength, "  DATA=", self.data, "  CKS=", format(self.checksum, "02x")
 
 
