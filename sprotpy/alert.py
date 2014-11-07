aa23a409c68d3e5425af63c7bad63e07f31f5140
adnan77 <v2102ap@gmail.com>
2014-11-07 14:53:12 +0800

    updated serial
    -removed redundant stuff in alert

--- sprotpy/alert.py
+++ sprotpy/alert.py
@@ -6,39 +6,18 @@ from easyNav_pi_dispatcher import DispatcherClient
 
 class Alert(object):
 
-    # Port for voice module
-    PORT_VOICE_MODULE = 9002
     PORT_NAV_MODULE = 9001
-    
-    ## Run Levels here
-    RUNLVL_NORMAL = 0
-    RUNLVL_WARNING_OBSTACLE = 1
     
     # Maximum sonar readings
     SONAR_MAX_DISTANCE = 700
-    
-    # Voice alerts
-    VOICE_ALERT_OBSTACLE_LEFT = "keep right"
-    VOICE_ALERT_OBSTACLE_RIGHT = "keep left"
-    VOICE_ALERT_STOP= "stop"
-    VOICE_ALERT_PROCEED = "proceed"
     
     OBSTACLE_RIGHT = 3
     OBSTACLE_LEFT = 2
     OBSTACLE_BOTH = 1
     OBSTACLE_NONE = 0    
-
-
-    # How much time to wait in secs before re-accessing situation and determining next movement
-    # (applicable in obstacle avoidance mode)
-    OBSTACLE_AVOIDANCE_WAIT_INTERVAL = 3
     
     # How close an object has to be before it is declared an obstacle (cm)
-    OBSTACLE_DIST_THRESHOLD = 30
-	
-    # How many times must obstacle be detected before it is confirmed
-    OBSTACLE_CONFIRMATION_COUNT = 3
-
+    OBSTACLE_DIST_THRESHOLD = 50
 
     def __init__(self):
         
@@ -48,32 +27,20 @@ class Alert(object):
 
         ## Attach event listeners upon instantiation (to prevent duplicates)
         self.attachEvents()        
-        
-        # Whether obstacle avoidance is in progress
-        self.obstacleAvoidanceInProgress = False
-                
-        # Keep track of what the last voice alert was so we don't irritate user unnecessarily!
-        self.lastVoiceAlert = None
 
         # Latest sonar readings
-	self.sonarLeft = Alert.SONAR_MAX_DISTANCE
+        self.sonarLeft = Alert.SONAR_MAX_DISTANCE
         self.sonarRight = Alert.SONAR_MAX_DISTANCE
-	
+    
   
     def start(self):
         ## Start inter-process comms
         self._dispatcherClient.start()      
-  	  
+        
     
     def sendAlert(self, alertType):
-        # Don't send successive stop commands - user may get angry!
-        #ignoreAlert = (self.lastVoiceAlert == Alert.VOICE_ALERT_STOP) and (alertType == Alert.VOICE_ALERT_STOP)
-        
-        #if (not ignoreAlert):
-            #self.lastVoiceAlert = alertType
-		print "Alert sent : ", alertType, "\n"
-            #self._dispatcherClient.send(Alert.PORT_VOICE_MODULE, 'say', {'text': alertType})
-		self._dispatcherClient.send(Alert.PORT_NAV_MODULE, 'obstacle', {'status': alertType})
+        print "Alert : ", alertType
+        self._dispatcherClient.send(Alert.PORT_NAV_MODULE, 'obstacle', {'status': alertType})
             
         
     # Check for obstacle position and returns OBSTACLE_X, where X is 
@@ -91,74 +58,37 @@ class Alert(object):
             return Alert.OBSTACLE_RIGHT
         else:
             return Alert.OBSTACLE_NONE
-        
-        
-    def performObstacleAvoidance(self):
-        hasObstacle = True
-	firstLoopRun = True
-
-        while (hasObstacle) :
-            # Wait for user to react - he's not "The Flash" you know!
-            time.sleep(Alert.OBSTACLE_AVOIDANCE_WAIT_INTERVAL)
-            currentObstacle = self.checkForObstacle()
-	
-	   
-            #if (currentObstacle == Alert.OBSTACLE_LEFT):
-                #voiceAlert = Alert.VOICE_ALERT_OBSTACLE_LEFT 
-            #    navAlert = Alert.VOICE_ALERT_OBSTACLE_LEFT             	
-	    #elif (currentObstacle == Alert.OBSTACLE_RIGHT):
-                #voiceAlert = Alert.VOICE_ALERT_OBSTACLE_LEFT 
-            #    navAlert = Alert.VOICE_ALERT_OBSTACLE_RIGHT 
-            #elif (currentObstacle == Alert.OBSTACLE_BOTH):
-                #voiceAlert = Alert.VOICE_ALERT_OBSTACLE_LEFT 
-            #    navAlert = Alert.VOICE_ALERT_STOP 
-            #else :
-            #    hasObstacle = False
-            #    voiceAlert = Alert.VOICE_ALERT_PROCEED
-
-            #self.sendAlert(voiceAlert)
-	    self.sendAlert(currentObstacle)
-
-	# We have finished the obstacle avoidance loop here
-        self.obstacleAvoidanceInProgress = False
 
 
     def convertStr(self,str):
-	maxIndex = 3
-	for i in range(3):
-		if(str[i].isdigit()):
-			maxIndex = i
+        maxIndex = 3
+        for i in range(3):
+            if(str[i].isdigit()):
+                maxIndex = i
 
-	return str[0:maxIndex+1]
+        return str[0:maxIndex+1]
 
 
     def attachEvents(self):
-	smokesignal.clear()
+        smokesignal.clear()
 
-    	@smokesignal.on('sonarData')
-    	def sonardata(args):
-		payload = eval(args.get("payload"))
-	        
-		strSonarLeft = self.convertStr(payload['1'])
-		self.sonarLeft = int(strSonarLeft)
-	        
-		strSonarRight = self.convertStr(payload['2'])
-		self.sonarRight = int(strSonarRight)
-	        print self.sonarLeft, self.sonarRight
+        @smokesignal.on('sonarData')
+        def sonardata(args):
+            payload = eval(args.get("payload"))
+            
+            strSonarLeft = self.convertStr(payload['1'])
+            self.sonarLeft = int(strSonarLeft)
+            
+            strSonarRight = self.convertStr(payload['2'])
+            self.sonarRight = int(strSonarRight)
 
-		self.sendAlert(self.checkForObstacle())
-        	#if( not self.obstacleAvoidanceInProgress ):
-        	#	if ( != Alert.OBSTACLE_NONE ):
-            #    		self.obstacleAvoidanceInProgress = True
-            #    		self.sendAlert(Alert.VOICE_ALERT_STOP)
-			#	sonarDataThread = threading.Thread(target=self.performObstacleAvoidance)
-	 		#	sonarDataThread.start()
- 		               	                
+            self.sendAlert(self.checkForObstacle())
+                                            
                     
 def runMain():
-	alert = Alert()
-	alert.start()
+    alert = Alert()
+    alert.start()
     
     
 if (__name__ == '__main__') :
-	runMain()
+    runMain()
--- sprotpy/serialmod.py
+++ sprotpy/serialmod.py
@@ -15,20 +15,20 @@ DEST_PORT_ALERT = 9004
 
 # Posts sonar data to server
 def sonar_post_process(ns) :
-	httpClient = req.RequestClass(local_mode=1)	
+    httpClient = req.RequestClass(local_mode=1)    
 
-	while (True) :
-		time.sleep(1)
-		if (ns.data != 0) :
-			try :
-				# Send right sonar HTTP request
-				httpClient.post_heartbeat_sonar("right", ns.data['right'])
-				# Send left sonar HTTP request
-				httpClient.post_heartbeat_sonar("left", ns.data['left'])
-				print "Sent sonar to server ", ns.data
-			except :
-				print "Server communications failed."
-				pass
+    while (True) :
+        time.sleep(1)
+        if (ns.data != 0) :
+            try :
+                # Send right sonar HTTP request
+                httpClient.post_heartbeat_sonar("right", ns.data['right'])
+                # Send left sonar HTTP request
+                httpClient.post_heartbeat_sonar("left", ns.data['left'])
+                print "Sent sonar to server ", ns.data
+            except :
+                print "Server communications failed."
+                pass
 
 
 # Extract sonar data from generic packet
@@ -60,100 +60,100 @@ p1 = multiprocessing.Process(target=sonar_post_process, args=(ns,))
 
 
 # easyNav-dispatcher communications
-def dispatchData() :			
-			
-	while True :
-		time.sleep(0.01)
-		
-		# No need to print every reading, some will do for debugging purpose
-		serialmod.canPrint = ((serialmod.iterationCount % serialmod.PRINT_EVERY_N_ITER) == 0)
-		
-		# Send compass data, set distance to zero if not available
-		if (serialmod.compassData != 0) :
-			# Acquire mutex lock on foot sensor data so that we don't miss any updates to it
-			serialmod.footsensMutex.acquire()
-			
-			# Package into cruncher data
-			cruncherData = { "angle" : serialmod.compassData, "distance" : serialmod.footsensData }			
-			serialmod.dc.send(DEST_PORT_CRUNCHER, 'angle', cruncherData)			
-			serialmod.compassData = 0
-			
-			# Clear footsens data if we have consumed it
-			if (serialmod.footsensData != 0) :
-				serialmod.footsensData = 0
-				
-			# Release the mutex lock after consuming footsend data
-			serialmod.footsensMutex.release()
-	
-			if (serialmod.canPrint) :
-				print "Foot/angle sensor ==> cruncher ", cruncherData
-			
-		# If both sonar data are available, combine them and send to alert
-		if ((serialmod.sonar1Data != 0) and (serialmod.sonar2Data != 0)) :
-			serialmod.sonar1Data.update(serialmod.sonar2Data)
-			combinedSonarData = serialmod.sonar1Data
-			
-			# Send to alert
-			serialmod.dc.send(DEST_PORT_ALERT, 'sonarData', combinedSonarData)
-			
-			if (serialmod.canPrint) :
-				print "Sonar ==> alert ", combinedSonarData
-			
-			# Send to child process for posting to HTTP server
-			#serialmod.ns.data = { 'left' : combinedSonarData['1'], 'right' : combinedSonarData['2']  }
-		
-			# Clear sonar data after we have consumed them
-			serialmod.sonar1Data = 0
-			serialmod.sonar2Data = 0
+def dispatchData() :            
+            
+    while True :
+        time.sleep(0.01)
+        
+        # No need to print every reading, some will do for debugging purpose
+        serialmod.canPrint = ((serialmod.iterationCount % serialmod.PRINT_EVERY_N_ITER) == 0)
+        
+        # Send compass data, set distance to zero if not available
+        if (serialmod.compassData != 0) :
+            # Acquire mutex lock on foot sensor data so that we don't miss any updates to it
+            serialmod.footsensMutex.acquire()
+            
+            # Package into cruncher data
+            cruncherData = { "angle" : serialmod.compassData, "distance" : serialmod.footsensData }            
+            serialmod.dc.send(DEST_PORT_CRUNCHER, 'angle', cruncherData)            
+            serialmod.compassData = 0
+            
+            # Clear footsens data if we have consumed it
+            if (serialmod.footsensData != 0) :
+                serialmod.footsensData = 0
+                
+            # Release the mutex lock after consuming footsend data
+            serialmod.footsensMutex.release()
+    
+            if (serialmod.canPrint) :
+                print "Foot/angle sensor ==> cruncher ", cruncherData
+            
+        # If both sonar data are available, combine them and send to alert
+        if ((serialmod.sonar1Data != 0) and (serialmod.sonar2Data != 0)) :
+            serialmod.sonar1Data.update(serialmod.sonar2Data)
+            combinedSonarData = serialmod.sonar1Data
+            
+            # Send to alert
+            serialmod.dc.send(DEST_PORT_ALERT, 'sonarData', combinedSonarData)
+            
+            if (serialmod.canPrint) :
+                print "Sonar ==> alert ", combinedSonarData
+            
+            # Send to child process for posting to HTTP server
+            #serialmod.ns.data = { 'left' : combinedSonarData['1'], 'right' : combinedSonarData['2']  }
+        
+            # Clear sonar data after we have consumed them
+            serialmod.sonar1Data = 0
+            serialmod.sonar2Data = 0
 
-			try:
-				# Count may overflow
-				serialmod.iterationCount += 1
-			except:
-				serialmod.iterationCount = 0
+            try:
+                # Count may overflow
+                serialmod.iterationCount += 1
+            except:
+                serialmod.iterationCount = 0
 
 # Run the easyNav dispatcher in a separate thread so it does not hog our serial read
 dispatcherThread = threading.Thread(target=serialmod.dispatchData)
-dispatcherThread.start()		
+dispatcherThread.start()        
 
 
 # Strips trailing zeroes if required
 def removeNullChars(str):
-	maxIndex = 7
-	for i in range(maxIndex):
-		if(str[i].isdigit() or str[i] == '.'):
-			maxIndex = i
+    maxIndex = 7
+    for i in range(maxIndex):
+        if(str[i].isdigit() or str[i] == '.'):
+            maxIndex = i
 
-	return str[0:maxIndex+1]
-	
-		
+    return str[0:maxIndex+1]
+    
+        
 while True :
 
-    	# Read a packet
-	pkt = sprotapi.SPROTReceive()    
+    # Read a packet
+    pkt = sprotapi.SPROTReceive()    
 
-	# Check for error
-	try :
-    		if (not isinstance(pkt, sprotpkt.SPROTPacket)) :
-        		print "recv error"
-    		else :
-			# Check packet type
-			#pkt.printPacket()
-			strpkt = pkt.data.decode("ascii")
+    # Check for error
+    try :
+            if (not isinstance(pkt, sprotpkt.SPROTPacket)) :
+                print "recv error"
+            else :
+            # Check packet type
+            #pkt.printPacket()
+            strpkt = pkt.data.decode("ascii")
 
-			if (strpkt[0] == b'1') :
-				sonar1Data = convertPacketToSonarData(strpkt)
-			elif (strpkt[0] == b'2') :
-				sonar2Data = convertPacketToSonarData(strpkt)
-        		elif (strpkt[0] == b'C') :
-				compassData = strpkt[2:5]
-			elif (strpkt[0] == b'F') :
-				footsensMutex.acquire()
-				footsensData = removeNullChars(strpkt[2:10])
-				footsensMutex.release()
-	except:
-		sprotapi.SPROTClose()
-		sprotapi.SPROTInit("/dev/ttyAMA0", baudrate=115200)
+            if (strpkt[0] == b'1') :
+                sonar1Data = convertPacketToSonarData(strpkt)
+            elif (strpkt[0] == b'2') :
+                sonar2Data = convertPacketToSonarData(strpkt)
+                elif (strpkt[0] == b'C') :
+                compassData = strpkt[2:5]
+            elif (strpkt[0] == b'F') :
+                footsensMutex.acquire()
+                footsensData = removeNullChars(strpkt[2:10])
+                footsensMutex.release()
+    except:
+        sprotapi.SPROTClose()
+        sprotapi.SPROTInit("/dev/ttyAMA0", baudrate=115200)
 
 
 # End of serialmod
